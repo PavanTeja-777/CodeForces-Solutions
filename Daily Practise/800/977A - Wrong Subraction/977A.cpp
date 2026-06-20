f5f09c3358b1001c15ced68ae8a28408128a51ad
Pavan Teja Sheela <psptsonofsantosh@gmail.com>
2026-06-20 22:12:45 +0530

    Add files via upload

---
+++ Daily Practise/800/977A - Wrong Subraction/977A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num, k;
+	cin >> num >> k;
+
+	while(k--){
+		if(num%10!=0) num--;
+		else num /= 10;
+	}
+	cout << num;
+
+    return 0;
+}
